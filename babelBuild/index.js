48268b4011648d34176d05f61cb5e021989b784c
Vikram Pratap Singh <vikram2327@gmail.com>
2023-02-10 12:31:15 +0530

    Hello India Build Add

--- babelBuild/index.js
+++ babelBuild/index.js
@@ -9,6 +9,12 @@ Object.defineProperty(exports, "GoodMorning", {
     return _GoodMorning.default;
   }
 });
+Object.defineProperty(exports, "HelloIndia", {
+  enumerable: true,
+  get: function get() {
+    return _HelloIndia.default;
+  }
+});
 Object.defineProperty(exports, "HelloWorld", {
   enumerable: true,
   get: function get() {
@@ -16,5 +22,6 @@ Object.defineProperty(exports, "HelloWorld", {
   }
 });
 var _HelloWorld = _interopRequireDefault(require("./HelloWorld"));
+var _HelloIndia = _interopRequireDefault(require("./HelloIndia"));
 var _GoodMorning = _interopRequireDefault(require("./GoodMorning"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
